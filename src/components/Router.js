c7a53cec5570c94e4ded676d905c81e96799a309
heereeh <rodbee20@gmail.com>
2022-08-21 11:02:54 +0900

    Logout / default redirect 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 export default function AppRouter({isLoggedIn}) {
   return <Router>
+    { isLoggedIn && <Navigation /> }
     <Routes>
-      { isLoggedIn? <Route exact path="/" element={<Home />} /> : <Route exact path="/" element={<Auth />} /> }
+      { isLoggedIn? (
+        <>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </>
+      ) : (
+        <>
+          <Route exact path="/" element={<Auth />} />
+        </>
+      )}
+      <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   </Router>
 }
